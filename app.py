35cc6fa48ccc2bfaa91033a864e53ef4aff949f3
ThiagoSouza-nes <thiago.souza2@br.nestle.com>
2026-07-16 11:57:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Mude para True para tirar o app do ar e False para voltar ao normal
-MODO_MANUTENCAO = True 
+MODO_MANUTENCAO = False 
 
 if MODO_MANUTENCAO:
     st.title("🚧 Portal MMD 🚧")
